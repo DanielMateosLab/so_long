bc5e77785be9f662dd27eff56d6e84f675a288aa
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-07-22 23:09:02 +0200

    Add error msgs defs

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 13:43:49 by damateos          #+#    #+#             */
-/*   Updated: 2024/07/20 13:46:48 by damateos         ###   ########.fr       */
+/*   Updated: 2024/07/22 23:01:02 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,15 @@
 
 # include "../libs/libft/include/libft.h"
 # include "../libs/MLX42/include/MLX42/MLX42.h"
+
+# define ARGC_ERR "Error\nInvalid args. Usage: ./so_long FILE_PATH\n"
+# define FILE_EXT_ERR "Error\nInvalid map extension. Allowed: .ber\n"
+# define EMPTY_MAP_ERR "Error\nEmpty map.\n"
+# define RECT_MAP_ERR "Error\nMap is not a rectangle.\n"
+# define COMPONENTS_ERR "Error\nForbidden component. Allowed: 0, 1, C, E, P.\n"
+# define WALL_ERR "Error\nThe map is not surrounded by walls\n"
+# define MIN_COMPS_ERR "Error\nMap should countain one exit,\
+ at least one object and the initial position\n"
+# define PATH_ERR "Error\nThere is not a valid path\n"
 
 #endif
