b8cb20f7f5bc03e15b1a282f59b3bc9431e6c048
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-07-22 23:09:27 +0200

    Add placeholder initialization. Start validating map

--- src/main.c
+++ src/main.c
@@ -6,13 +6,70 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 12:54:44 by damateos          #+#    #+#             */
-/*   Updated: 2024/07/20 13:48:52 by damateos         ###   ########.fr       */
+/*   Updated: 2024/07/22 23:08:48 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
+#include <stdio.h>
 
-int	main(void)
+#define WIDTH 512
+#define HEIGHT 512
+
+static mlx_image_t* image;
+
+void ft_action_keys_hook(void* param)
 {
-	return (0);
+	mlx_t* mlx = param;
+
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+	if (mlx_is_key_down(mlx, MLX_KEY_UP) || mlx_is_key_down(mlx, MLX_KEY_W))
+		image->instances[0].y -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_DOWN) || mlx_is_key_down(mlx, MLX_KEY_S))
+		image->instances[0].y += 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_LEFT) || mlx_is_key_down(mlx, MLX_KEY_A))
+		image->instances[0].x -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT) || mlx_is_key_down(mlx, MLX_KEY_D))
+		image->instances[0].x += 5;
 }
+
+int is_valid_extension(char *file_name)
+{
+	return (ft_strncmp(file_name + ft_strlen(file_name) - 4, ".ber", 4) == 0);
+}
+
+int main(int argc, char **argv)
+{
+	mlx_t	*mlx;
+
+	if (argc != 2)
+		return (ft_printf(ARGC_ERR), 1);
+	if (!is_valid_extension(argv[1]))
+		return (ft_printf(FILE_EXT_ERR), 1);
+	// TODO: continue
+	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", 0)))
+	{
+		ft_printf("%s", mlx_strerror(mlx_errno));
+		return (EXIT_FAILURE);
+	}
+	if (!(image = mlx_new_image(mlx, 128, 128)))
+	{
+		mlx_close_window(mlx);
+		ft_printf("%s", mlx_strerror(mlx_errno));
+		return (EXIT_FAILURE);
+	}
+	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
+	{
+		mlx_close_window(mlx);
+		ft_printf("%s", mlx_strerror(mlx_errno));
+		return (EXIT_FAILURE);
+	}
+
+	mlx_loop_hook(mlx, ft_action_keys_hook, mlx);
+
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
+}
+
